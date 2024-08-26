6e983420ed5e887349f7fed4a9d0e67290676cfc
Santiago Bruno <soysantiagobruno@gmail.com>
2024-08-26 04:15:14 -0300

    creo componente registrar-usuario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SaberMasComponent } from './components/saber-mas/saber-mas.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { PublicarMascotaComponent } from './components/publicar-mascota/publicar-mascota.component';
 import { HomeUsuarioComponent } from './components/home-usuario/home-usuario.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
@@ -12,6 +13,7 @@ export const routes: Routes = [
     {path: 'saber-mas', component: SaberMasComponent},
     {path: 'publicar-mascota', component: PublicarMascotaComponent},
     {path: 'home-usuario', component: HomeUsuarioComponent}, //home para adoptar
+    {path: 'registrar-usuario', component: RegistrarUsuarioComponent},
 
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: PageNotFoundComponent }
---
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-registrar-usuario',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './registrar-usuario.component.html',
+  styleUrl: './registrar-usuario.component.css'
+})
+export class RegistrarUsuarioComponent {
+
+}
