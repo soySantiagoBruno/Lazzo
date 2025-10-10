2fd324dbf04de6c773ce985500232cc532de8a19
Santiago Bruno <soysantiagobruno@gmail.com>
2025-10-09 21:21:48 -0300

    creo componente filtro y about

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,13 +5,14 @@ import { InfoMascotaComponent } from "../info-mascota/info-mascota.component";
 import { NavbarComponent } from "../navbars/navbar/navbar.component";
 import { RouterLink } from '@angular/router';
 import { FiltroComponent } from '../filtro/filtro.component';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [FooterComponent, CarruselComponent, InfoMascotaComponent, FiltroComponent, NavbarComponent, RouterLink]
+    imports: [FooterComponent, CarruselComponent, InfoMascotaComponent, FiltroComponent, NavbarComponent, RouterLink, AboutComponent, AboutComponent]
 })
 export class HomeComponent {
 
---
+++ src/app/mocks/mock-cards.ts
@@ -0,0 +1,26 @@
+export const MOCK_CARDS = [
+  {
+    id: 1,
+    nombre: 'Luna',
+    edad: 3,
+    telefono: '+54 9 261 555-1234',
+    tipo: 'Perro',
+    imagen: 'https://example.com/luna.jpg'
+  },
+  {
+    id: 2,
+    nombre: 'Milo',
+    edad: 2,
+    telefono: '+54 9 261 555-5678',
+    especie: 'Gato',
+    imagen: 'https://example.com/milo.jpg'
+  },
+  {
+    id: 3,
+    nombre: 'Kira',
+    edad: 5,
+    telefono: '+54 9 261 555-9012',
+    especie: 'Conejo',
+    imagen: 'https://example.com/kira.jpg'
+  }
+];
