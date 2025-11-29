78e34e55b7580719dcfacdd9bd72366ae412e551
Santiago Bruno <soysantiagobruno@gmail.com>
2025-11-29 17:45:20 -0300

    creo dto de provincia y departamento

---
+++ src/app/models/departamento-dto.ts
@@ -0,0 +1,5 @@
+export interface DepartamentoDto {
+    id: string;
+    nombre: string;
+    idProvincia: string;
+}
---
+++ src/app/models/provincia-dto.ts
@@ -0,0 +1,4 @@
+export interface ProvinciaDto {
+    id: string;
+    nombre: string;
+}
