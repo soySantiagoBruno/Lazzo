3ba5a4a14a37e8e6dcbaa1f48b5e6e2ad2dc1ef3
Santiago Bruno <soysantiagobruno@gmail.com>
2025-11-29 00:28:14 -0300

    style filtro

--- src/app/components/filtro/filtro.component.ts
+++ src/app/components/filtro/filtro.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-filtro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './filtro.component.html',
   styleUrl: './filtro.component.css'
 })
 export class FiltroComponent {
+
+  ubicacion = new FormControl('');
+
+
+aplicarFiltro() {
+  console.log("Filtro aplicado!");
+
+  /* const collapseEl = document.getElementById('collapseExample');
+  const collapse = bootstrap.Collapse.getOrCreateInstance(collapseEl);
+  collapse.hide(); */
+}
+
 
 }
